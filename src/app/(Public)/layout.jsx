e7f70987fe9232ecb8889ad38d9b2f70a0858ae0
novrialsandi <novrialsandi@gmail.com>
2025-05-25 21:11:14 +0700

    chore: Comment out unused imports and components in layout and page files

--- src/app/(Public)/layout.jsx
+++ src/app/(Public)/layout.jsx
@@ -1,13 +1,13 @@
-import Layout from "@/lib/layout/public/Index";
-// import { GoogleAnalytics } from "@next/third-parties/google";
+// import Layout from "@/lib/layout/public/Index";
+// // import { GoogleAnalytics } from "@next/third-parties/google";
 
-const PublicLayout = ({ children }) => {
-	return (
-		<Layout>
-			{children}
-			{/* <GoogleAnalytics gaId="G-920REJG2GY" /> */}
-		</Layout>
-	);
-};
+// const PublicLayout = ({ children }) => {
+// 	return (
+// 		<Layout>
+// 			{children}
+// 			{/* <GoogleAnalytics gaId="G-920REJG2GY" /> */}
+// 		</Layout>
+// 	);
+// };
 
-export default PublicLayout;
+// export default PublicLayout;
--- src/app/(Public)/page.jsx
+++ src/app/(Public)/page.jsx
@@ -1,60 +1,59 @@
-import Link from "next/link";
-import { cookies } from "next/headers";
-import LandingContent from "@/lib/views/public/Index";
+// import Link from "next/link";
+// import LandingContent from "@/lib/views/public/Index";
 
-export const metadata = {
-	title: "Songolikur - Media To Discover Yogyakarta Culture & Football",
-	description: "Discover Yogyakarta Culture & Football",
-	metadataBase: new URL("https://songolikur.vercel.app"),
-	openGraph: {
-		type: "website",
-		url: "https://songolikur.vercel.app/",
-		title: "Songolikur - Media To Discover Yogyakarta Culture & Football",
-		description: "Discover Yogyakarta Culture & Football",
-		images: [
-			{
-				url: "/meta.png",
-				width: 1200,
-				height: 630,
-			},
-		],
-	},
-	twitter: {
-		card: "summary_large_image",
-		title: "Songolikur - Media To Discover Yogyakarta Culture & Football",
-		description: "Discover Yogyakarta Culture & Football",
-		images: ["/meta.png"],
-	},
-};
+// export const metadata = {
+// 	title: "Songolikur - Media To Discover Yogyakarta Culture & Football",
+// 	description: "Discover Yogyakarta Culture & Football",
+// 	metadataBase: new URL("https://songolikur.vercel.app"),
+// 	openGraph: {
+// 		type: "website",
+// 		url: "https://songolikur.vercel.app/",
+// 		title: "Songolikur - Media To Discover Yogyakarta Culture & Football",
+// 		description: "Discover Yogyakarta Culture & Football",
+// 		images: [
+// 			{
+// 				url: "/meta.png",
+// 				width: 1200,
+// 				height: 630,
+// 			},
+// 		],
+// 	},
+// 	twitter: {
+// 		card: "summary_large_image",
+// 		title: "Songolikur - Media To Discover Yogyakarta Culture & Football",
+// 		description: "Discover Yogyakarta Culture & Football",
+// 		images: ["/meta.png"],
+// 	},
+// };
 
-const LandingPage = async () => {
-	const cookieStore = await cookies();
-	const cid = cookieStore.get("cid");
+// const LandingPage = async () => {
+// 	const cookieStore = await cookies();
+// 	const cid = cookieStore.get("cid");
 
-	const cookieData = cid ? JSON.parse(cid.value) : null;
+// 	const cookieData = cid ? JSON.parse(cid.value) : null;
 
-	return (
-		<>
-			<LandingContent />
-			{/* <div className="w-full h-screen flex justify-center items-center">
-				{cookieData ? (
-					<Link
-						href="/dashboard"
-						className="p-2 bg-primary text-secondary rounded-md"
-					>
-						Go to Dashboard
-					</Link>
-				) : (
-					<Link
-						href="/login"
-						className="p-2 bg-primary text-secondary rounded-md"
-					>
-						Login
-					</Link>
-				)}
-			</div> */}
-		</>
-	);
-};
+// 	return (
+// 		<>
+// 			<LandingContent />
+// 			{/* <div className="w-full h-screen flex justify-center items-center">
+// 				{cookieData ? (
+// 					<Link
+// 						href="/dashboard"
+// 						className="p-2 bg-primary text-secondary rounded-md"
+// 					>
+// 						Go to Dashboard
+// 					</Link>
+// 				) : (
+// 					<Link
+// 						href="/login"
+// 						className="p-2 bg-primary text-secondary rounded-md"
+// 					>
+// 						Login
+// 					</Link>
+// 				)}
+// 			</div> */}
+// 		</>
+// 	);
+// };
 
-export default LandingPage;
+// export default LandingPage;
